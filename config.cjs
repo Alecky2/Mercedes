d6cf4c7d343c0a05a0ceae14dd6ab4c22a9258b5
Alecky2 <manaleki22@gmail.com>
2024-12-07 02:29:09 +0300

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -3,7 +3,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "Your Session",
+  SESSION_ID: process.env.SESSION_ID || "Ethix-MD&WgZDKubV",
   PREFIX: process.env.PREFIX || '.',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true,
   AUTOLIKE_STATUS: process.env.AUTOLIKE_STATUS !== undefined ? process.env.AUTOLIKE_STATUS === 'true' : false,
@@ -23,9 +23,9 @@ const config = {
   REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || "public",
-  OWNER_NAME: process.env.OWNER_NAME || "Marisel",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "254740007567",
-  SUDO_NUMBERS: process.env.SUDO_NUMBERS || "254740007567,254790375710",
+  OWNER_NAME: process.env.OWNER_NAME || "Alecky",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "254797666750",
+  SUDO_NUMBERS: process.env.SUDO_NUMBERS || "254797666750,254742880195",
   GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyCUPaxfIdZawsKZKqCqJcC-GWiQPCXKTDc",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
 };
